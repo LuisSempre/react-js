6f97f8e49d4653eb403e811664b77abcc1bacaf0
LuisSempre <luis.sempre@viannasempre.com.br>
2023-08-11 21:04:55 -0300

    feat: Add Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React, { useState } from "react"
+import Button from "./Button"
+
 function App() {
+const [total, setTotal ] = React.useState(0)
+
+function incrementar() {
+		setTotal((total) => total +1)
+	}
+
   return (
-      <div class="text-purple-500 flex h-screen w-full justify-center items-center bg-black font-bold">
-			<p>HI</p>
+      <div className="text-purple-500 flex flex-col h-screen w-full justify-center items-center bg-black font-bold">
+			<p>Total: {total}</p>
+			<button onClick={incrementar} className="px-4 rounded-full bg-purple-500 text-white">Incrementar</button>
+			<Button />
     </div>
   )
 }
