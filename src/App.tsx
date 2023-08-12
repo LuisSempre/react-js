9850a86f3db450c45e4355e5d056a4704da288e8
LuisSempre <luis.sempre@viannasempre.com.br>
2023-08-12 03:10:18 -0300

    feat: Add fetch Shopping

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,44 @@
-import React, { useState } from "react"
-import Button from "./Button"
+import React from "react";
+import Input from "./Input";
 
-type User = {
-	name: string;
-	pro: string
-}
-
-function user() {
-	return {
-		name: "Luis",
-		pro: "Dev"
-	}
-}
+type Venda = {
+  id: string;
+  nome: string;
+  preco: number;
+  status: string;
+};
 
 function App() {
-const [data, setData] = React.useState<null | User>(null)
-const [total, setTotal] = React.useState(0)
+  const [inicio, setInicio] = React.useState("");
+  const [final, setFinal] = React.useState("");
+  const [data, setData] = React.useState<null | Venda[]>(null);
 
-React.useEffect(() => {
-	setTimeout(() => {
-		setData(user())
-	}, 1000)
-}, [])
+  React.useEffect(() => {
+    if (inicio !== "" && final !== "") {
+      fetch(`https://data.origamid.dev/vendas/?inicio=${inicio}&final=${final}`)
+        .then((r) => r.json())
+        .then((json) => setData(json as Venda[]))
+        .catch((error) => console.log(error));
+    }
+  }, [inicio, final]);
+
   return (
-	<div>
-		<div>
-			<p>Total: {total}</p>
-			<Button add={setTotal} />
-		</div>
-   <div>{data !== null && <div>{data.name}: {data.pro}</div>}</div>
-	 </div>
-  )
+    <div>
+      <div>
+        <Input label="Início" type="date" setState={setInicio} value={inicio} />
+        <Input label="Final" type="date" setState={setFinal} value={final} />
+      </div>
+      <ul>
+        {data !== null &&
+          data.map((venda) => (
+            <li key={venda.id}>
+              {venda.nome}: {venda.status}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
